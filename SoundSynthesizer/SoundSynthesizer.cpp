f2bbdd7e3a4a87b7953ed37bbeba34576c408f38
glitchhopcore <mercyfortrash@gmail.com>
2023-10-31 17:35:42 +0530

    created basic initialization code

--- SoundSynthesizer/SoundSynthesizer.cpp
+++ SoundSynthesizer/SoundSynthesizer.cpp
@@ -2,9 +2,19 @@
 //
 
 #include <iostream>
+using namespace std;
 #include "olcNoiseMaker.h"
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	//Get a list of all sound hardware, Enumerate uses mmepi.h command to get the count of connected audio devices and then iterates over them returning as a vector of strings
+	vector<wstring> devices = olcNoiseMaker<short>::Enumerate();
+	
+	//Display all the found devices
+	for (auto d : devices)
+		wcout << "Found Object Device: " << d << endl;
+
+	//Create a sound instance
+	olcNoiseMaker<short> sound(devices[0], 44100, 1, 8, 512);
+
 }
