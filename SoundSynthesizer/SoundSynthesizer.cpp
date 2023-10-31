83d5f41b0c83cd3e119fe75bc821d17322792ac2
glitchhopcore <mercyfortrash@gmail.com>
2023-10-31 15:55:51 +0530

    Add project files.

---
+++ SoundSynthesizer/SoundSynthesizer.cpp
@@ -0,0 +1,10 @@
+// SoundSynthesizer.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include "olcNoiseMaker.h"
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
