a0ab4862135fc7a8497ea43e6ac9daa95d246e53
glitchhopcore <mercyfortrash@gmail.com>
2023-11-03 19:46:36 +0530

    Basic Keyboard Implementation

--- SoundSynthesizer/SoundSynthesizer.cpp
+++ SoundSynthesizer/SoundSynthesizer.cpp
@@ -5,10 +5,13 @@
 using namespace std;
 #include "olcNoiseMaker.h"
 
+// Global variable that gets updated, responsible for setting output frequency
+double dFrequencyOutput = 0.0;
+
 // Creating a function to change the shape of the waveform that we have created, dTime tells about the time that has elapsed.
 double MakeNoise(double dTime) {
 	// Returning a sine wave function with 440Hz being the note 'A'
-	return 0.5 * sin(440.0 * 2 * PI * dTime);
+	return 0.2 * sin(dFrequencyOutput * 2 * PI * dTime);
 }
 
 int main()
@@ -27,8 +30,23 @@ int main()
 	// Linking MakeNoise with the created sound instance
 	sound.SetUserFunction(MakeNoise);
 
-	// Adding a while loop to keep the program running, will change this later
+	const double dOctaveBaseFrequency = 110.0; // Base Frequency of the A2 Note
+	const double d12thRootOf2 = pow(2.0, 1.0 / 12.0); // Since 12 Notes per octave and the frequency doubles
+
 	while (1) {
-	
+		// Adding a keyboard control to the sound played 
+		
+		bool bKeyPressed = false;
+		for (int k = 0; k < 15; k++) {
+			// The 0x8000 bit of GetAsyncKeyState gives you the real time state of the key 
+			if (GetAsyncKeyState((unsigned char)("ZSXCFVGBNJMK\xbcL\xbe"[k])) & 0x8000) {
+				dFrequencyOutput = dOctaveBaseFrequency * pow(d12thRootOf2, k);
+				bKeyPressed = true;
+			}
+		}
+
+		if(!bKeyPressed) {
+			dFrequencyOutput = 0.0;
+		}
 	}
 }
