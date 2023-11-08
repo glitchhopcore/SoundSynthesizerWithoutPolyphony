c98ec39137a2ce81addb3bf8a4f5d7eef7e30349
glitchhopcore <mercyfortrash@gmail.com>
2023-11-08 19:38:54 +0530

    will add functionality for Attack, Decay, Sustain and Release in future

--- SoundSynthesizer/SoundSynthesizer.cpp
+++ SoundSynthesizer/SoundSynthesizer.cpp
@@ -13,11 +13,30 @@ double w(double dHertz) {
 	return dHertz * 2.0 * PI;
 }
 
+double osc(double dHertz, double dTime, int nType) {
+	switch(nType) {
+	case 0: // Sine Wave
+		return sin(w(dHertz) * dTime);
+	case 1: // Square Wave
+		return sin(w(dHertz) * dTime) > 0.0 ? 1.0 : -1.0;
+	case 2: // Triangle Wave
+		return asin(sin(w(dHertz) * dTime)) * (2.0/PI);
+	case 3: // Saw Wave 
+		return (2.0 / PI) * (dHertz * PI * fmod(dTime, 1.0 / dHertz) - (PI / 2.0));
+	case 4:
+		return 2.0 * ((double)rand() / (double)(RAND_MAX)) - 1.0;
+	default: 
+		return 0.0;
+	}
+}
+
 // Creating a function to change the shape of the waveform that we have created, dTime tells about the time that has elapsed.
 double MakeNoise(double dTime) {
-	// Returning sine wave with a given frequency, dTime representing the time elapsed
-	return 0.2 * sin(w(dFrequencyOutput) * dTime);
+	// Returning oscillators, based on the type provided to the function 
+	double dOutput = osc(dFrequencyOutput, dTime, 3);
+	return dOutput * 0.2; // Master Volume
 }
+
 
 int main()
 {
@@ -51,7 +70,7 @@ int main()
 		// Adding a keyboard control to the sound played 
 		
 		bool bKeyPressed = false;
-		for (int k = 0; k < 15; k++) {
+		for (int k = 0; k <= 15; k++) {
 			// The 0x8000 bit of GetAsyncKeyState gives you the real time state of the key 
 			if (GetAsyncKeyState((unsigned char)("ZSXCFVGBNJMK\xbcL\xbe\xbf"[k])) & 0x8000) {
 				dFrequencyOutput = dOctaveBaseFrequency * pow(d12thRootOf2, k);
