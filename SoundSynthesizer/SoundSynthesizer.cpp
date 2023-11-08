9caa242068dddeb4c636cf58805992048fb8fd08
glitchhopcore <mercyfortrash@gmail.com>
2023-11-08 21:37:09 +0530

    Envelopes were implemented

--- SoundSynthesizer/SoundSynthesizer.cpp
+++ SoundSynthesizer/SoundSynthesizer.cpp
@@ -5,8 +5,11 @@
 using namespace std;
 #include "olcNoiseMaker.h"
 
-// Global variable that gets updated, responsible for setting output frequency
-double dFrequencyOutput = 0.0;
+// Global Variables
+atomic<double> dFrequencyOutput = 0.0; 
+const double dOctaveBaseFrequency = 110.0; // Base Frequency of the A2 Note
+const double d12thRootOf2 = pow(2.0, 1.0 / 12.0); // Since 12 Notes per octave and the frequency doubles
+sEnvelopeADSR envelope;
 
 //Function for converting Hertz to Angular Frequency for math functions 
 double w(double dHertz) {
@@ -32,11 +35,87 @@ double osc(double dHertz, double dTime, int nType) {
 
 // Creating a function to change the shape of the waveform that we have created, dTime tells about the time that has elapsed.
 double MakeNoise(double dTime) {
-	// Returning oscillators, based on the type provided to the function 
-	double dOutput = osc(dFrequencyOutput, dTime, 3);
+	// Returning oscillators, based on the type provided to the function, here we can also use osc part as a composite of more than 1 function generations
+	double dOutput = envelope.GetAmplitude(dTime) * osc(dFrequencyOutput, dTime, 3); 
 	return dOutput * 0.2; // Master Volume
 }
 
+struct sEnvelopeADSR {
+	double dAttackTime;
+	double dDecayTime;
+	double dReleaseTime;
+
+	
+	double dSustainAmplitude;
+	
+	// The amplitude of the initial attack may go to a different amplitude than the one it will rest at, thus to keep track of the starting amplitude
+	double dStartAmplitude;
+	
+	// Times for when the user has pressed and released the key, based on which the envelope duration will be decided
+	double dTriggerOnTime;
+	double dTriggerOffTime;
+
+	// State of Key 
+	bool bNoteOn;
+
+	// Default Values
+	sEnvelopeADSR() {
+		dAttackTime = 0.01;
+		dDecayTime = 0.01;
+		dReleaseTime = 0.02;
+		dSustainAmplitude = 0.8;
+		dStartAmplitude = 0.02;
+		dTriggerOnTime = 0.0;
+		dTriggerOffTime = 0.0;
+		bNoteOn = false;
+	}
+
+	// The Envelope can be indexed at any point in time by the make noise function, thus we should return the amplitude of the function for the given time
+
+	double GetAmplitude(double dTime) {
+		double dAmplitude = 0.0;
+		// The time after the note was pressed 
+		double dLifeTime = dTime - dTriggerOnTime;
+		if (bNoteOn) {
+			// Attack
+			if (dLifeTime <= dAttackTime) {
+				dAmplitude = (dLifeTime / dAttackTime) * dStartAmplitude;
+			}
+			// Decay
+			if (dLifeTime > dAttackTime && dLifeTime <= (dAttackTime+dDecayTime)) {
+				dAmplitude = ((dLifeTime - dAttackTime)/ dDecayTime) * (dSustainAmplitude - dStartAmplitude) + dStartAmplitude;
+			}
+			// Sustain 
+			if (dLifeTime > (dAttackTime + dDecayTime)) {
+				dAmplitude = dSustainAmplitude;
+			}
+		}
+		else {
+			// Release
+			dAmplitude = ((dTime - dTriggerOffTime) / (dReleaseTime)) * (0.0 - dSustainAmplitude) + dSustainAmplitude;
+		}
+
+		// When dAmplitude is too small, set it to zero
+		if (dAmplitude <= 0.0001) {
+			dAmplitude = 0;
+		}
+
+		return dAmplitude;
+	}
+
+	// Methods for Note ON and OFF
+	
+	void NoteOn(double dTime) {
+		dTriggerOnTime = dTime;
+		bNoteOn = true;
+	}
+	
+	void NoteOff(double dTime) {
+		dTriggerOffTime = dTime;
+		bNoteOn = false;
+	}
+
+};
 
 int main()
 {
@@ -63,8 +142,6 @@ int main()
 	// Linking MakeNoise with the created sound instance
 	sound.SetUserFunction(MakeNoise);
 
-	const double dOctaveBaseFrequency = 110.0; // Base Frequency of the A2 Note
-	const double d12thRootOf2 = pow(2.0, 1.0 / 12.0); // Since 12 Notes per octave and the frequency doubles
 
 	while (1) {
 		// Adding a keyboard control to the sound played 
@@ -74,12 +151,14 @@ int main()
 			// The 0x8000 bit of GetAsyncKeyState gives you the real time state of the key 
 			if (GetAsyncKeyState((unsigned char)("ZSXCFVGBNJMK\xbcL\xbe\xbf"[k])) & 0x8000) {
 				dFrequencyOutput = dOctaveBaseFrequency * pow(d12thRootOf2, k);
+				envelope.NoteOn(sound.GetTime());
 				bKeyPressed = true;
 			}
 		}
 
 		if(!bKeyPressed) {
 			dFrequencyOutput = 0.0;
+			envelope.NoteOff(sound.GetTime());
 		}
 	}
 }
